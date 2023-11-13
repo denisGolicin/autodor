b3369dc5d7f2496baa967faba6330bf24bc1a9b8
DGDeveloper <111699872+denisGolicin@users.noreply.github.com>
2023-11-13 14:33:27 +0200

    Update changeModel.py

--- unit/changeModel.py
+++ unit/changeModel.py
@@ -2,7 +2,7 @@ import requests
 from config_loader import load_config
 
 protocol, domain, port, url = load_config()
-
+#sync
 opt = requests.get(url=f'{url}/sdapi/v1/options')
 opt_json = opt.json()
 opt_json['sd_model_checkpoint'] = 'uberRealisticPornMerge_urpmv12.safetensors' # change name model
